6a6e786829312cb09a9d11429b4a52235fd6d210
Sanju Pethani <sanjanapethani@gmail.com>
2023-09-11 08:31:33 +0530

    - First commit.

---
+++ AI_Tool/AI_Tool/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  AI_Tool
+//
+//  Created by sanjana pethani on 11/09/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
